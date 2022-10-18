dbca0d64f29b627a4c430297799dd5435b6649ee
casfalking <casfalking@pop-os.localdomain>
2022-10-18 20:16:31 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+class DevelopmentConfig():
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_EHCO = False
+    DEBUG = True
+    SQL_ALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wsr29yhi@localhost/blog'
+    DATABASE_CONNECT_OPTIONS = {}
+    JSON_AS_ASCII = False
+
