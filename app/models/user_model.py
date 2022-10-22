a6cc3fc912a5d74b074bad4200620ca5f56eeea6
casfalking <casfalking@pop-os.localdomain>
2022-10-22 02:59:52 -0300

    feat db methods user_model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,16 +1,63 @@
 from . import db, ma
-from sqlalchemy import true
+from sqlalchemy import true, BigInteger, String, DateTime
 from marshmallow import fields
-
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 #SQLAlchemy Model
 class User(db.Model):
         __tablename__ = 'Users'
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(255), nullable=False)
-        username = db.Column(db.String(255), nullable=False)
-        email = db.Column(db.String(255), nullable=False, unique=True)
-        password = db.Column(db.String(255))
+        id = db.Column(BigInteger, primary_key=True)
+        name = db.Column(String(255), nullable=False)
+        username = db.Column(String(255), nullable=False)
+        email = db.Column(String(255), nullable=False, unique=True)
+        password = db.Column(String(255))
+        created_at = db.Column(DateTime)
+        modifield_at = db.Column(DateTime)
 
+        def __init__(self, name, email, password, username) -> None:
+            self.name = name
+            self.email= email
+            self.password = generate_password_hash(password)
+            self.username = username
+            self.created_at = datetime.datetime.utcnow()
+            self.modified_at = datetime.datetime.utcnow()
+
+        def save(self):
+            db.session.add(self)
+            db.session.commit()
+        
+        def update(self, data):
+            for key, item in data.items():
+                if key == 'password':
+                    self.password = generate_password_hash(data.get('password'))
+                
+                setattr(self, key, item)
+            
+            self.modified_at = datetime.datetime.utcnow()
+
+            db.session.commit()
+        
+        def delete(self):
+            db.session.delete(self)
+            db.session.commit()
+
+        @staticmethod
+        def get_one_user(id):
+            return User.query.get(id)
+
+        @staticmethod
+        def get_all_users():
+            return User.query.all()
+
+        @staticmethod
+        def get_user_by_email(email):
+            return User.query.filter_by(email=email).firt()
+
+        def verify_password(self, data):
+            return check_password_hash(self.password, data.get('password'))
+
+
+        
 #Marshmallow Serealize
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
