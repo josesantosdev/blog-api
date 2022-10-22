09b29a024041c8ac50d6b67409bae48d924f5afa
casfalking <casfalking@pop-os.localdomain>
2022-10-22 02:32:48 -0300

    feat user_model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,24 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from sqlalchemy import true
+from marshmallow import fields
 
+
+db = SQLAlchemy
+ma = Marshmallow
 
 def condigure(app):
-    db = SQLAlchemy
-    ma = Marshmallow
     db.init_app(app)
     ma.init_app(app)
     app.db = db
     
 
 class User(db.Model):
-    def __init__(self, id, name, email, password) -> None:
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
+        __tablename__ = 'Users'
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(255), nullable=False)
+        username = db.Column(db.String(255), nullable=False)
+        email = db.Column(db.String(255), nullable=False, unique=True)
+        password = db.Column(db.String(255))
 
-class UserSchema:
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        pass
+        model = User
+        load_instance = true
+
+        id = fields.Integer()
+        name = fields.Str(required=True)
+        username = fields.Str(required=True)
+        email = fields.Str(required=True)
+        password = fields.Str(required=True)
+
+        
+        
 
     
