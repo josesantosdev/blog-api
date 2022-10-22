66831482cca0e3649d038f032c629343a3845c9b
casfalking <casfalking@pop-os.localdomain>
2022-10-22 03:22:58 -0300

    deleting secret information

--- config.py
+++
@@ -1,8 +0,0 @@
-class DevelopmentConfig():
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_EHCO = False
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wsr29yhi@localhost/blog'
-    DATABASE_CONNECT_OPTIONS = {}
-    JSON_AS_ASCII = False
-
