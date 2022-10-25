80b8bba1f7eaa1df9859f843ac2259a2405b2c54
casfalking <casfalking@pop-os.localdomain>
2022-10-25 03:04:07 -0300

    feat auth_controller register method

---
+++ app/controllers/auth_controller.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, request, Response, json
+from app import db, jwt
+from app.models.user_model import User, UserSchema
+from app.models.revoked_token_model import RevokedToken
+
+
+
+class AuthController:
+    
+    auth_controller = Blueprint(name='auth_controller', import_name=__name__)
+    
+
+    #Loading the jwt identity value
+    @jwt.user_identity_loarder
+    def user_identity_lookup(login):
+        return login
+
+    #cheking if token is revoked
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_data):
+        jti = jwt_data['jti']
+        rt = RevokedToken.query.filter_by(jti=jti).frist()
+        return bool(rt)
+
+    @auth_controller.route('/register', methods=['POST'])
+    def register():
+        req_data = request.get_json()
+        data, error = user_schema.load(req_data)
+
+        if error:
+            return custom_response(error, 400)
+
+        #checking if user already exust in db
+        user_in_db = User.get_user_by_email(data.get('email'))
+        if user_in_db:
+            message = {'error':'User already exist, please supply another email adress'}
+            return custom_response(message, 400)
+
+        user = User(data)
+        user.save()
+        ser_data = user_schema.dump(user).data
+        return custom_response(ser_data, 201)
+
+
+
+
+user_schema = UserSchema()
+
+def custom_response(res, status_code):
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
